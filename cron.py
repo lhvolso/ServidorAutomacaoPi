5a1f392dea2d25da7b2b8d80359cf78b90e52dfc
Luiz Henrique Volso <luiz@MacBook-Pro-de-Luiz.local>
2018-03-06 20:16:27 -0300

    Corrigido funcionalidade de agendamento via CRON

--- cron.py
+++ cron.py
@@ -5,8 +5,8 @@ from datetime import datetime
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-with open("/home/pi/ServidorAutomacaoPi/cron.log","a") as d:
-	d.write(str(datetime.now())+" - Controle!\n")
+# with open("/home/pi/ServidorAutomacaoPi/cron.log","a") as d:
+	# d.write(str(datetime.now())+" - Controle!\n")
 
 tempoexecuta = datetime.now().time()
 
@@ -27,7 +27,7 @@ with open("/home/pi/ServidorAutomacaoPi/cron.json","r") as j:
 			
 			pinoretorno = int(json[indice]['pinoretorno'])
 			GPIO.setup(pinoretorno, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-			if GPIO.input(pinoretorno) == 1:
+			if GPIO.input(pinoretorno) == 0:
 				horaCron = int(tempDesliga.split(':', 1)[0])
 				minutoCron = int(tempDesliga.split(':', 1)[1])
 				minutoTolerancia = int(minutoCron) + 1
@@ -53,7 +53,7 @@ with open("/home/pi/ServidorAutomacaoPi/cron.json","r") as j:
 
 			pinoretorno = int(json[indice]['pinoretorno'])
 			GPIO.setup(pinoretorno, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-			if GPIO.input(pinoretorno) == 0:
+			if GPIO.input(pinoretorno) == 1:
 				horaCron = int(tempLiga.split(':', 1)[0])
 				minutoCron = int(tempLiga.split(':', 1)[1])
 				minutoTolerancia = int(minutoCron) + 1
